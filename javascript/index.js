d31de9e8d26fc79e427b23286d46b17a1294862d
Shannon Chen <shannon.chen1@wccschools.org>
2025-06-09 21:29:43 +0000

    Mon, Jun 9, 2025, 2:29 PM -07:00

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,17 @@
 let line = "CATch them if you can!";
-        for (i = 0; i < line.length; i++) {
-            setTimeout((function(i) {
-                return function() {
-                    document.querySelector("#slogan").innerHTML += line[i];
-                };
-            })(i), 40 * i);
-        }
+for (i = 0; i < line.length; i++) {
+    setTimeout((function(i) {
+        return function() {
+            document.querySelector("#slogan").innerHTML += line[i];
+        };
+    })(i), 40 * i);
+}
 
-        setInterval(function() {
-            let slogan = document.querySelector("#slogan");
-            if (slogan.style.visibility === "hidden") {
-                slogan.style.visibility = "visible";
-            } else {
-                slogan.style.visibility = "hidden";
-            }
-        }, 1000);
+setInterval(function() {
+    let slogan = document.querySelector("#slogan");
+    if (slogan.style.visibility === "hidden") {
+        slogan.style.visibility = "visible";
+    } else {
+        slogan.style.visibility = "hidden";
+    }
+}, 1000);
